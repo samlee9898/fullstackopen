b2fed075ac9c2fe89a2247000be1f0a8854c6c42
samlee9898 <samyeol9898@gmail.com>
2026-03-03 22:23:48 -0600

    3.9 done and working on 3.10

---
+++ part3/phonebook_backend/backend/index.js
@@ -0,0 +1,112 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+
+morgan.token('data', function(req, res) {
+    if (req.method == 'POST') {
+        return JSON.stringify(req.body)
+    }
+    return ' '
+})
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(400).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+// logic for generating a random number
+const maxNum = Number.MAX_SAFE_INTEGER
+function getRandomInt(maxNum) {
+    return Math.floor(Math.random() * maxNum)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name and/or number missing'
+        })
+    }
+
+    if (persons.some(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const newId = getRandomInt(maxNum)
+    const newPerson = {
+        "id": `${newId}`,
+        "name": `${request.body.name}`,
+        "number":`${request.body.number}`
+    }
+    // saving new person to the persons array
+    persons = persons.concat(newPerson)
+
+    // sending a response to the client
+    return response.status(201).json(newPerson)
+    
+})
+
+// binding
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
